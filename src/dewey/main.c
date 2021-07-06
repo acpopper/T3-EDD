83e06162ce9121293e8d876b53311c302aff2b97
acpopper <acpopper@uc.cl>
2021-07-06 16:29:39 -0400

    read files

--- src/dewey/main.c
+++ src/dewey/main.c
@@ -10,11 +10,36 @@ int main(int argc, char** argv)
         printf("Modo de uso: %s <network.txt> <output.txt>\n", argv[0]);
         return 1;
     }
+    /* Abrimos el archivo de input */
+    FILE *input_file = fopen(argv[1], "r");
+    /* Abrimos el archivo de output */
+    FILE *output_file = fopen(argv[2], "w");
 
-    /* ------------- POR IMPLEMENTAR -------------- */
-    /* El código de tu tarea va aquí.               */
+    int n_clientes;
+    fscanf(input_file, "%d", &n_clientes);
+    int n_cd;
+    fscanf(input_file, "%d", &n_cd);
+    int n_edges;
+    fscanf(input_file, "%d", &n_edges);
+
+    printf("%i %i %i\n", n_clientes, n_cd, n_edges);
+
+    int inicio;
+    int destino;
+    int costo;
+    for (int i = 0; i < n_edges; i++)
+    {
+        fscanf(input_file, "%d", &inicio);
+        fscanf(input_file, "%d", &destino);
+        fscanf(input_file, "%d", &costo);
+        
+        printf("%i %i %i\n", inicio, destino, costo);
+     
+    }
 
 
+    fclose(input_file);
+    fclose(output_file);
     // Terminamos exitosamente
     return 0;
 }
