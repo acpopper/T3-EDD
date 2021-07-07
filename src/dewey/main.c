4037e623b47efedf842be3f1befa99f6eb309cc7
acpopper <acpopper@uc.cl>
2021-07-06 22:41:40 -0400

    problem in kruskal

--- src/dewey/main.c
+++ src/dewey/main.c
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
     int n_edges;
     fscanf(input_file, "%d", &n_edges);
 
-    // printf("%i %i %i\n", n_clientes, n_cd, n_edges);
+    printf("%i %i %i\n", n_clientes, n_cd, n_edges);
 
     int V = n_clientes+n_cd;
     int E = n_edges;
@@ -36,17 +36,17 @@ int main(int argc, char** argv)
         fscanf(input_file, "%d", &inicio);
         fscanf(input_file, "%d", &destino);
         fscanf(input_file, "%d", &costo);
-        // printf("%i %i %i\n", inicio, destino, costo);
-        graph->edges[0].src = inicio;
-        graph->edges[0].dest = destino;
-        graph->edges[0].weight = costo;
+        graph->edges[i].src = inicio;
+        graph->edges[i].dest = destino;
+        graph->edges[i].weight = costo;
+        printf("%i %i %i\n", graph->edges[i].src, graph->edges[i].dest, graph->edges[i].weight);
     }
 
-    Edge* resultado = malloc(V*sizeof(Edge));
-    KruskalMST(graph);
+    // Edge* resultado = malloc(V*sizeof(Edge));
+    // KruskalMST(graph);
 
-    destroy_graph(graph);
-    free(resultado);
+    // destroy_graph(graph);
+    // free(resultado);
     fclose(input_file);
     fclose(output_file);
     // Terminamos exitosamente
